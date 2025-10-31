b9243e7c2043068a554a4b1b45385b323b4f9390
Your Name <you@example.com>
2025-10-31 14:11:10 +0300

    chore: Update site favicon to new brand icon (favicon.png) via gatsby-plugin-manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -21,7 +21,7 @@ module.exports = {
         background_color: `#ffffff`,
         theme_color: `#f6911e`,
         display: `standalone`,
-        icon: `src/assets/images/profile.png`, // Changed to profile.png
+        icon: `src/assets/images/favicon.png`,
         icon_options: {
           purpose: `any maskable`,
         },
